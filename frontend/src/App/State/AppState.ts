ec44e1c513837dda84c9f9a4803c5f28b34bbe2d
Mark McDowall <mark@mcdowall.ca>
2025-12-21 20:28:59 -0800

    Use react-query for manual import

--- frontend/src/App/State/AppState.ts
+++ frontend/src/App/State/AppState.ts
@@ -1,7 +1,6 @@
 import BlocklistAppState from './BlocklistAppState';
 import CaptchaAppState from './CaptchaAppState';
 import ImportSeriesAppState from './ImportSeriesAppState';
-import InteractiveImportAppState from './InteractiveImportAppState';
 import OAuthAppState from './OAuthAppState';
 import ProviderOptionsAppState from './ProviderOptionsAppState';
 import SettingsAppState from './SettingsAppState';
@@ -10,7 +9,6 @@ interface AppState {
   blocklist: BlocklistAppState;
   captcha: CaptchaAppState;
   importSeries: ImportSeriesAppState;
-  interactiveImport: InteractiveImportAppState;
   oAuth: OAuthAppState;
   providerOptions: ProviderOptionsAppState;
   settings: SettingsAppState;
--- frontend/src/Store/Actions/index.js
+++ frontend/src/Store/Actions/index.js
@@ -1,6 +1,5 @@
 import * as captcha from './captchaActions';
 import * as importSeries from './importSeriesActions';
-import * as interactiveImportActions from './interactiveImportActions';
 import * as oAuth from './oAuthActions';
 import * as providerOptions from './providerOptionActions';
 import * as settings from './settingsActions';
@@ -8,7 +7,6 @@ import * as settings from './settingsActions';
 export default [
   captcha,
   importSeries,
-  interactiveImportActions,
   oAuth,
   providerOptions,
   settings
